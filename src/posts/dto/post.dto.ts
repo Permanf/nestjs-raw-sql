75570893b3504606ac81369a58c2b9ebd8924b1a
Permanf <tirkishmyradovperman@gmail.com>
2024-08-31 12:21:40 +0500

    72.Working with Postgresql using sql queries

---
+++ src/posts/dto/post.dto.ts
@@ -0,0 +1,14 @@
+
+import { IsString, IsNotEmpty } from 'class-validator';
+ 
+class PostDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+ 
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+}
+ 
+export default PostDto;
---
+++ src/posts/post.model.ts
@@ -0,0 +1,11 @@
+
+import { Expose } from 'class-transformer';
+ 
+class PostModel {
+  id: number;
+  title: string;
+  @Expose({ name: 'post_content' })
+  content: string;
+}
+ 
+export default PostModel;
